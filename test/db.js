2742e84d1f23fa21ac3ff3eb15fa51137288d31e
James Gosnell <jamesgosnell@gmail.com>
2015-06-04 04:52:51 -0500

    REMOVED no longer needed code because of sqlite3 load script

--- test/db.js
+++ test/db.js
@@ -1,26 +1,13 @@
 const SqliteToJson = require('sqlite-to-json');
 const sqlite3 = require('sqlite3');
 const db = new SqliteToJson({
-	  //client: new sqlite3.Database(__dirname+'/../db/cdr.sqlite3')
-	client: new sqlite3.Database(":memory:")
-});
-db.tables(function (err, tables) {
-	// all your table names here 
+	client: new sqlite3.Database(__dirname+'/../db/cdr.sqlite3')
 });
 
 db.client.serialize(function() {
-	db.client.run("CREATE TABLE lorem (info TEXT, val INT)");
-
-	var stmt = db.client.prepare("INSERT INTO lorem VALUES (?,?)");
-	for (var i = 0; i < 10; i++) {
-		stmt.run("Ipsum " + i, i+1);
-	}
-	stmt.finalize();
-
-	db.client.each("SELECT rowid AS id, info, val FROM lorem", function(err, row) {
+	db.client.each("SELECT * FROM cdr", function(err, row) {
 		console.dir(row);
 	});
-	db.client.run("DROP TABLE lorem");
 });
 
  
