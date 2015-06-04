735aebf6d67d928d07a26d0b11257d6927a08399
James Gosnell <jamesgosnell@gmail.com>
2015-06-04 04:55:12 -0500

    REMOVED sqlite3-to-json; no longer needed

--- test/db.js
+++ test/db.js
@@ -1,14 +1,10 @@
-const SqliteToJson = require('sqlite-to-json');
 const sqlite3 = require('sqlite3');
-const db = new SqliteToJson({
-	client: new sqlite3.Database(__dirname+'/../db/cdr.sqlite3')
-});
+const db = new sqlite3.Database(__dirname+'/../db/cdr.sqlite3');
 
-db.client.serialize(function() {
-	db.client.each("SELECT * FROM cdr", function(err, row) {
+db.serialize(function() {
+	db.each("SELECT * FROM cdr", function(err, row) {
 		console.dir(row);
 	});
 });
 
- 
-db.client.close();
+db.close();
