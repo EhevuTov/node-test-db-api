556f94f3810900a182185688335a05278779ec56
James Gosnell <jamesgosnell@gmail.com>
2015-06-11 18:08:07 -0500

    ADDED updated Dojo code and CSS

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 require([
     "dojo/dom"
+  , "dojo/dom-style"
   , "dojo/parser"
   , "dojo/data/ObjectStore"
   , "dojo/store/Cache"
@@ -10,21 +11,21 @@ require([
   , "dijit/layout/TabContainer"
   , "dijit/layout/ContentPane"
   , "dijit/registry"
-  , "dojox/grid/DataGrid"
+//  , "dojox/grid/DataGrid"
 	, "dgrid/Grid"
 //	, "dgrid"
   , "dojo/domReady!"]
-  , function(q,w,e,r,t,y,u,i,a,s,d,f,dgrid,grid) {dojo.ready( ready ) } ); // the callback function to run when done asynchronously
+  , ready ); // the callback function to run when done asynchronously
 
-function ready(q,w,e,r,t,y,u,i,a,s,d,f,dgrid,Grid) {
+function ready(dom,style,w,e,r,t,y,u,i,a,s,d,f,Grid) {
   // init needed to begin program after successful loading
   // run loading icon for start of program
-  var n = dojo.byId("preLoader");
+  var n = dom.byId("preLoader");
   dojo.fadeOut({
 		node:n,
 		duration:720,
 		onEnd:function(){
-			dojo.style(n,"display","none");
+			style.set(n,"display","none");
 		}
 	}).play();
 
@@ -34,39 +35,14 @@ function ready(q,w,e,r,t,y,u,i,a,s,d,f,dgrid,Grid) {
 		{ first: 'Pat', last: 'Sajak', age: 65 }
 	];
 
+	/*
 	var grid = new Grid({
 		columns: {
-							 first: 'First Name',
+			first: 'First Name',
 			last: 'Last Name',
 			age: 'Age'
-						 }
+		}
 	}, 'grid');
 	grid.renderArray(data);
-
-/*
-var store = [{
-    0: "Lab 46", 1: "DA1", 2: "130.020.002", 3: "4721", 4: "130.020.001"
-  , 5: "A01", 6: "3456", 7: "", 8: "2012-09-27 21:40:25", 9: "I", 10: "1"
-  , 11: "2129991721", 12: "6305799800", 13: "6309756721", 14: "181.275"
-  , 15: "0.000", 16: "268536255", 17: "", 18: "", 19: "F", 20: "6305799880"
-}];
-*/
-
-//create the store with the data
-
-//var cdrStore = new dojo.store.Memory();
-//var dataStore = new dojo.data.ObjectStore({objectStore: cdrStore});
-//var dataStore = dojo.store.Cache(dojo.store.JsonRest({target:"/filter/"}), dojo.store.Memory());
-//console.dir(store);
-//console.dir(dataStore.get(1));
-//dojo.store.JsonRest({target:"/filter/"}).then(function(results){ console.dir(results);});
-//var store = dojo.store.JsonRest({target:"/filter/"}).get();
-//console.dir(store);
-//dojo.store.JsonRest({target:"/filter/"}).get().then(function(results){ console.dir(results.filters);});
-// wrap the store with Observable to make it possible to monitor
-//cdrStore = new dojo.store.Observable(cdrStore);
-
-//dijit.registry.byId('grid').setStore(dataStore);
-
-// Socket.IO connection
-};
+	*/
+}
