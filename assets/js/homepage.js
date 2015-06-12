9a2c760a88921c83d8fce6315c9121eea00f61f5
James Gosnell <jamesgosnell@gmail.com>
2015-06-12 01:29:01 -0500

    ADDED JSON request to populate grid

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,27 +1,20 @@
 require([
-    "dojo/dom"
-  , "dojo/dom-style"
-  , "dojo/parser"
-  , "dojo/data/ObjectStore"
-  , "dojo/store/Cache"
-  , "dojo/store/Memory"
-  , "dojo/store/Observable"
-  , "dojo/store/JsonRest"
-  , "dijit/layout/BorderContainer"
-  , "dijit/layout/TabContainer"
-  , "dijit/layout/ContentPane"
-  , "dijit/registry"
-//  , "dojox/grid/DataGrid"
-	, "dgrid/Grid"
-//	, "dgrid"
-  , "dojo/domReady!"]
-  , ready ); // the callback function to run when done asynchronously
+		"dojo/dom"
+	, "dojo/dom-style"
+	, "dojo/_base/fx"
+	, 'dgrid/OnDemandGrid'
+	, 'dstore/RequestMemory'
+	, "dijit/layout/BorderContainer"
+	, "dijit/layout/ContentPane"
+	, "dijit/layout/TabContainer"
+	, "dojo/domReady!"]
+	, ready ); // the callback function to run when done asynchronously
 
-function ready(dom,style,w,e,r,t,y,u,i,a,s,d,f,Grid) {
-  // init needed to begin program after successful loading
-  // run loading icon for start of program
-  var n = dom.byId("preLoader");
-  dojo.fadeOut({
+function ready(dom,style,fx,Grid,RequestMemory) {
+	// init needed to begin program after successful loading
+	// run loading icon for start of program
+	var n = dom.byId("preLoader");
+	fx.fadeOut({
 		node:n,
 		duration:720,
 		onEnd:function(){
@@ -29,20 +22,19 @@ function ready(dom,style,w,e,r,t,y,u,i,a,s,d,f,Grid) {
 		}
 	}).play();
 
-	var data = [
-		{ first: 'Bob', last: 'Barker', age: 89 },
-		{ first: 'Vanna', last: 'White', age: 55 },
-		{ first: 'Pat', last: 'Sajak', age: 65 }
-	];
+	var columnHeaders = {
+			UnitID: 'Unit ID',
+			Disposition: 'Disposition',
+			OPC: 'OPC',
+			TCIC: 'TCIC',
+			DPC: 'DPC',
+			LinkSet: 'LinkSet'
+	};
 
-	/*
+	// load Grid
 	var grid = new Grid({
-		columns: {
-			first: 'First Name',
-			last: 'Last Name',
-			age: 'Age'
-		}
+		collection: new RequestMemory({ target:'/cdr' }),
+		columns: columnHeaders
 	}, 'grid');
-	grid.renderArray(data);
-	*/
+	grid.startup();
 }
